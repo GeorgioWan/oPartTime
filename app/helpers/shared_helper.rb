9b4abc70113ae7b88f0ed78f5197f2db5509ac50
GeorgioWan <georgio.wan@icatchinc.com>
2015-11-05 18:38:03 +0800

    add alerttify, edit show and index page :sunny:

---
+++ app/helpers/shared_helper.rb
@@ -0,0 +1,23 @@
+module SharedHelper
+  ALERT_TYPES = [:error, :info, :success, :warning] unless const_defined?(:ALERT_TYPES)
+
+  def alertify_notice
+    jsReturn = javascript_tag()
+    flash.each do |type, message|
+      type = type.to_sym unless type.nil?
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      next unless ALERT_TYPES.include?(type)
+
+      js_alertify = ""
+      Array(message).each do |msg|
+        js_alertify << "alertify.logPosition('top right'); alertify.#{type}('#{msg}');\n" if msg;
+      end
+      jsReturn = javascript_tag(js_alertify)
+    end
+    jsReturn.html_safe()
+  end
+end
