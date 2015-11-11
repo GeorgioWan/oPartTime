3c144c23d3ee4bc6249ced9c915885370de2de0f
GeorgioWan <georgio.wan@icatchinc.com>
2015-11-11 17:06:38 +0800

    add validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,8 @@ class Job < ActiveRecord::Base
   belongs_to :user
 
   ### Validate
-  validates_presence_of   :title, :description
+  validates_presence_of   :title, :description, :pay, :location
   validates_uniqueness_of :title
+  validates_length_of     [:title, :company], in: 3..15
+  validates_numericality_of :pay, only_integer: true
 end
