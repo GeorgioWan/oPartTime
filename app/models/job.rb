2122b3804df3cefd830b7b6106ef017888746f00
GeorgioWan <georgio.wan@icatchinc.com>
2015-11-11 18:04:01 +0800

    update validte:+1:

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,5 +6,7 @@ class Job < ActiveRecord::Base
   validates_presence_of   :title, :description, :pay, :location
   validates_uniqueness_of :title
   validates_length_of     [:title, :company], in: 3..15
-  validates_numericality_of :pay, only_integer: true
+  validates_numericality_of :pay, only_integer: true, message: "請填入整數"
+  validates_format_of :url, with: /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix,
+                      message: "URL 格式不正確"
 end
