618f46950ee392e92ef027e23288a0409b572663
GeorgioWan <georgio_sky2000@hotmail.com>
2015-12-14 07:31:11 +0000

    Add privacy and policy page.

---
+++ app/helpers/oparttime_helper.rb
@@ -0,0 +1,2 @@
+module OparttimeHelper
+end
---
+++ test/controllers/oparttime_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OparttimeControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
