cdebcef3b2f2adcf56b9677233028a6435e6a62c
GeorgioWan <georgio_sky2000@hotmail.com>
2015-12-17 10:26:10 +0000

    Adjust city selector tob

--- app/helpers/map_svg_helper.rb
+++ app/helpers/map_svg_helper.rb
@@ -1,6 +1,6 @@
 module MapSvgHelper
   def active_path id, city, drawpath
     active = request.path == city ? :active : nil
-    content_tag :path, "", d: drawpath, id: id, data: {city: city}, class: active
+    content_tag :path, "", d: drawpath, id: id, data: {city: city}, class: ["city-section", active]
   end
 end
