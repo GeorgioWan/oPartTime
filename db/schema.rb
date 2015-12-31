a181c3caef77322c7b7f3d12207bb8600ed9cb94
GeorgioWan <georgio.wan@gmail.com>
2015-12-31 08:29:57 +0000

    Users url use friendly_id now!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231052917) do
+ActiveRecord::Schema.define(version: 20151231080621) do
 
   create_table "favorite_jobs", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -73,9 +73,11 @@ ActiveRecord::Schema.define(version: 20151231052917) do
     t.string   "facebook"
     t.string   "googleplus"
     t.string   "website"
+    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
 
 end
