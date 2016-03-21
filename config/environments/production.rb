feae072eb3490005a1f65d279991d34ff001bc7f
GeorgioWan <georgio_sky2000@hotmail.com>
2016-03-21 05:43:39 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -79,5 +79,6 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: "oparttime.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 end
