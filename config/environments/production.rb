7556c9b888b18094509b58a6b2f6039263bb7b8c
GeorgioWan <georgio_sky2000@hotmail.com>
2016-03-21 03:39:25 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: "http://www.oparttime.com" }
+  config.action_mailer.default_url_options = { host: "oparttime.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 end
