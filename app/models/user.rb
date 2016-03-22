9176bb2f4d1bff9072806f45f9ee2cf33a3f5986
GeorgioWan <georgio_sky2000@hotmail.com>
2016-03-22 18:08:37 +0000

    Fix bug :beetle:

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   
   ### omniauth facebook
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.id).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid      = auth.uid
       user.email    = auth.info.email
