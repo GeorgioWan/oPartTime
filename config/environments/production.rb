eb878848408dc063cde8ead4942f977f64fdc87e
GeorgioWan <georgio_sky2000@hotmail.com>
2016-05-04 13:58:54 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: "http://www.oparttime.com" }
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 end
